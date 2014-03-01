bf947c4ce43751f802d75e39522ec2c3eaaf4c7d
Dan Tao <daniel.tao@gmail.com>
2014-03-01 06:51:51 -0800

    added nearestColor.from
    plus a bunch of comments

--- nearestColor.js
+++ nearestColor.js
@@ -1,25 +1,52 @@
 var Color = require('color');
 
-var predefinedColors;
+/**
+ * Defines an available color.
+ *
+ * @typedef {Object} ColorSpec
+ * @property {string=} name A name for the color, e.g., 'red'
+ * @property {string} source The hex-based color string, e.g., '#FF0'
+ * @property {RGB} rgb The {@link RGB} color values
+ */
 
 /**
- * Gets the nearest color, from the list supplied by calling setColors.
+ * Describes a matched color.
+ *
+ * @typedef {Object} ColorMatch
+ * @property {string} name The name of the matched color, e.g., 'red'
+ * @property {string} value The hex-based color string, e.g., '#FF0'
+ */
+
+/**
+ * Provides the RGB breakdown of a color.
+ *
+ * @typedef {Object} RGB
+ * @property {number} r The red component, from 0 to 255
+ * @property {number} g The green component, from 0 to 255
+ * @property {number} b The blue component, from 0 to 255
+ */
+
+/**
+ * Gets the nearest color, from the given list of {@link ColorSpec} objects
+ * (which defaults to {@link nearestColor.DEFAULT_COLORS}).
+ *
+ * Probably you wouldn't call this method directly. Instead you'd get a custom
+ * color matcher by calling {@link nearestColor.from}.
+ *
+ * @public
+ * @param {string} hex The hex-based color string, e.g., '#FF0'
+ * @param {Array.<ColorSpec>=} colors An optional list of available colors
+ *     (defaults to {@link nearestColor.DEFAULT_COLORS})
+ * @return {ColorMatch|string} If the colors in the provided list had names,
+ *     then a {@link ColorMatch} object with the name and (hex) value of the
+ *     nearest color from the list. Otherwise, simply the hex value.
  *
  * @example
- * var customColors = {
- *   'maroon': '#800',
- *   'light yellow': '#ffe',
- *   'pale blue': '#def'
- * };
- *
  * nearestColor('#f11'); // => '#f00'
  * nearestColor('#f88'); // => '#f80'
  * nearestColor('#ffe'); // => '#ff0'
  * nearestColor('#efe'); // => '#ff0'
  * nearestColor('#abc'); // => '#808'
- *
- * nearestColor('#f00', customColors);
- * // => { name: 'maroon', value: '#800' }
  */
 function nearestColor(hex, colors) {
   var needle = Color(hex).rgb(),
@@ -28,7 +55,7 @@ function nearestColor(hex, colors) {
       rgb,
       value;
 
-  colors = mapColors(colors || nearestColor.DEFAULT_COLORS);
+  colors || (colors = nearestColor.DEFAULT_COLORS);
 
   for (var i = 0; i < colors.length; ++i) {
     rgb = colors[i].rgb;
@@ -50,6 +77,60 @@ function nearestColor(hex, colors) {
     value.source;
 }
 
+/**
+ * Provides a matcher to find the nearest color based on the provided list of
+ * available colors.
+ *
+ * @public
+ * @param {Array.<string>|Object} availableColors An array of hex-based color
+ *     strings, or an object mapping color *names* to hex values.
+ * @return {function(string):ColorMatch|string} A function with the same
+ *     behavior as {@link nearestColor}, but with the list of colors predefined.
+ *
+ * @example
+ * var getColor = nearestColor.from({
+ *   'maroon': '#800',
+ *   'light yellow': '#ffe',
+ *   'pale blue': '#def'
+ * });
+ *
+ * var getBGColor = getColor.from([
+ *   '#eee',
+ *   '#444'
+ * ]);
+ *
+ * getColor('#f00');
+ * // => { name: 'maroon', value: '#800' }
+ *
+ * getBGColor('#fff'); // => '#eee'
+ * getBGColor('#000'); // => '#444'
+ */
+nearestColor.from = function from(availableColors) {
+  var colors = mapColors(availableColors),
+      nearestColorBase = nearestColor;
+
+  var matcher = function nearestColor(hex) {
+    return nearestColorBase(hex, colors);
+  };
+
+  // Keep the 'from' method, to support changing the list of available colors
+  // multiple times without needing to keep a reference to the original
+  // nearestColor function.
+  matcher.from = from;
+
+  return matcher;
+};
+
+/**
+ * Given either an array or object of colors, returns an array of
+ * {@link ColorSpec} objects (with {@link RGB} values).
+ *
+ * @private
+ * @param {Array.<string>|Object} colors An array of hex-based color strings, or
+ *     an object mapping color *names* to hex values.
+ * @return {Array.<ColorSpec>} An array of {@link ColorSpec} objects
+ *     representing the same colors passed in.
+ */
 function mapColors(colors) {
   if (colors instanceof Array) {
     return colors.map(function(hex) {
@@ -71,7 +152,12 @@ function mapColors(colors) {
   return result;
 };
 
-nearestColor.DEFAULT_COLORS = [
+/**
+ * Default colors. Comprises the colors of the rainbox (good ol' ROY G. BIV).
+ * This list will be used for calls to {@nearestColor} that don't specify a list
+ * of available colors to match.
+ */
+nearestColor.DEFAULT_COLORS = mapColors([
   '#f00', // r
   '#f80', // o
   '#ff0', // y
@@ -79,6 +165,6 @@ nearestColor.DEFAULT_COLORS = [
   '#00f', // b
   '#008', // i
   '#808'  // v
-];
+]);
 
 module.exports = nearestColor;
